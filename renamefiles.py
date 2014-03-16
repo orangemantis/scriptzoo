4a3bdf25bebcd2a81324688271072f6754425e5a
orangemantis <ron@orangemantis.net>
2014-03-15 21:57:32 -0400

    Changed things a bit
    Added error handling and logging.
    Added file numbering.

--- renamefiles.py
+++ renamefiles.py
@@ -1,18 +1,42 @@
 #Renames files, wrote it to rename images, If I had my Mac, I would have used Automator.
 
 import os
-import shutil
-fileLoc = ''#set to dir path of files to be renamed
-newPrefix = 'some_pic'#set this to new name prefix of files
+fileLoc = '/USers/Tace/Desktop/family'
+newPrefix = 'family_pics_'#set new file prefix here
 filesList = os.listdir(path=fileLoc)
 manifest = ''
+picCount = 1
+errorLog = ''
 
 for file in filesList:
     fileName = fileLoc + '/' + file
-    manifest += file + ':: description: ... \r\n'
-    shutil.copyfile(fileName, fileLoc + '/' + newPrefix + file)
-    os.remove(fileName)
+    ext = file.split('.')
+    
+    if (len(ext) > 1):
+        ext = ext[1]
+    else:
+        ext = ''
+    
+    newFileName = fileLoc + '/' + newPrefix + str(picCount) + '.' + ext
+    manifest += newFileName + ':: description: ... \r\n'
 
-manifestFile = open(fileLoc + '/' + 'descriptions.txt' ,'w')
+    try:
+        if not (newPrefix in file and 'errorLog' in file):
+            os.rename(fileName, newFileName)
+            picCount += 1
+        else:
+            raise Exception(file + ' has already been renamed.')
+    except Exception as e:
+        if (e):
+            errorLog = str(e) + '\r\n'
+        else:
+            errorLog += 'Error with file: ' + file + '.\r\n'
+
+manifestFile = open(fileLoc + '/' + newPrefix + '.txt' ,'w')
 manifestFile.write(manifest)
 manifestFile.close()
+
+if (errorLog):
+    errorFile = open(fileLoc + '/' + 'errorlog.txt', 'w')
+    errorFile.write(errorLog)
+    errorFile.close()
